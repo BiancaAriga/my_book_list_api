eaf7c3757db9bc0422d00cf17d8f75dff591b554
Bianca Ariga <biancaariga45@gmail.com>
2026-07-02 19:17:09 -0300

    fix: cors

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "null",
         "http://127.0.0.1:5500",
-        "http://127.0.0.1:5501",
         "http://localhost:5500",
     ],
     allow_credentials=True,
